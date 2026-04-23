05002e2f38262715481f16a917f5f3da170be179
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-04-23 22:56:35 +0600

    up

---
+++ private/controllers/home.php
@@ -0,0 +1,12 @@
+<?php 
+    //  home controller 
+    class Home extends Controller
+    {
+        public function __construct()
+        {
+            // code ...
+             echo "this is home controller";
+        }
+    }
+
+    
---
+++ private/controllers/students.php
@@ -0,0 +1,12 @@
+<?php 
+    //  students controller 
+    class Students extends Controller
+    {
+        public function __construct()
+        {
+            // code ...
+             echo "this is Students controller";
+        }
+    }
+
+    
