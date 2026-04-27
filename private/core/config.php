5ad82322e7caf1404919bb1a5940298bbb3a7280
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-04-27 22:57:22 +0600

    up

---
+++ private/core/config.php
@@ -0,0 +1,3 @@
+<?php 
+
+    define('Root', 'http://localhost/school/public');
--- private/views/home.view.php
+++ private/views/home.view.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cloudflare.com">
+<link href="https://jsdelivr.net" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
 </head>
 <body>
 
-    <h1>This is the home page</h1>
+    <div class="container-fluid">
+        <i class="bi bi-search"></i>
+        <h1>  This is Home</h1>
+    </div>
     
 </body>
 </html>
