db97832e48bf1f0413e992949f5833911940a656
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-04-27 23:02:21 +0600

    Update home.view.php

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cloudflare.com">
-<link href="https://jsdelivr.net" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
 
 </head>
